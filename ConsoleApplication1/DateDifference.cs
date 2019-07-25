172848bc2798a83562cea216580f5f54bc7c0a82
VishnuRckr <53258761+VishnuRckr@users.noreply.github.com>
2019-07-25 12:56:16 +0530

    Add files via upload
    Included methods and rectified

--- ConsoleApplication1/DateDifference.cs
+++ ConsoleApplication1/DateDifference.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Globalization;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -8,54 +9,74 @@ namespace ConsoleApplication1
 {
     class Program
     {
-        static void Main(string[] args)
+        public static void Main(string[] args)
         {
-            int days, months, years;
-            string Dob;
-            Console.WriteLine("enter the date of birth in YYYY/MM/DD format : ");
-            Dob = Console.ReadLine();        
-            try
+            string ch = "y";
+            do
             {
-                DateTime Dob1 = Convert.ToDateTime(Dob);
-                DateTime CurrentDate = DateTime.Now;
-                if (Dob1.Year > CurrentDate.Year)
-                {
-                    Console.WriteLine("Please enter a proper date ");
-                }
-                if (CurrentDate.Year > Dob1.Year)
-                {
-                    years = CurrentDate.Year - Dob1.Year;
-                }
-                else
-                {
-                    years = Dob1.Year - CurrentDate.Year;
-                }
-                if (CurrentDate.Month > Dob1.Month)
-                {
-                    months = CurrentDate.Month - Dob1.Month;
-                }
-                else
-                {
-                    months = Dob1.Month - CurrentDate.Month;
-                }
-                if (CurrentDate.Day > Dob1.Day)
-                {
-                    days = CurrentDate.Day - Dob1.Day;
-                }
-                else
-                {
-                    days = Dob1.Day - CurrentDate.Day;
-                }
 
-                Console.WriteLine("The Difference is {0} days {1} months {2} years", days, months, years);
-            }
-            catch (FormatException)
-            {
-                Console.WriteLine("Please enter a proper date ");
-            }
+                string db = DateOfBirth();
+                DateTime dt = ValidateDob(db);
+                string output = Age(dt);
+                Console.WriteLine(output);
+                Console.WriteLine("Do you want to continue(y/n)?");
+                ch = Console.ReadLine();
+            } while (ch == "y");
+            
+        }
+        public static string DateOfBirth()
+        {
+            Console.WriteLine("\nEnter your date of brirth seperated by '/' : ");
+            string dob = Console.ReadLine();
+            return dob;
+        }
 
+        public static DateTime ValidateDob(string dob)
+        {
+
+            DateTime dob1;
+            if (DateTime.TryParseExact(dob, "dd/MM/yyyy", CultureInfo.InvariantCulture, DateTimeStyles.None, out dob1))
+             {
+                if (dob1 > DateTime.Now)
+                 {
+                    Console.WriteLine("Please Enter a Proper Date");
+                    return DateTime.MinValue;
+                 }
+                return dob1;
+             }
+            else
+             {
+                Console.WriteLine("Not a date");
                 Console.ReadLine();
+                Environment.Exit(0);
+                return DateTime.MinValue;
+             }
+        }
+
+        public static string Age(DateTime dob1)
+        {
+            DateTime Now = DateTime.Now;
+            int years = new DateTime(DateTime.Now.Subtract(dob1).Ticks).Year - 1;
+            DateTime d = dob1.AddYears(years);
+            int months = 0;
+            for(int i = 1; i <= 12; i++)
+            {
+                if (d.AddMonths(i) == Now)
+                {
+                    months = i;
+                    break;
+                }
+                else if(d.AddMonths(i) >= Now)
+                {
+                    months = i - 1;
+                    break;
+                }
+            }
+            int days = Now.Subtract(d.AddMonths(months)).Days;
+            return String.Format("The Age is : {0} years {1} months {2} days",years,months,days);
+
         }
     }
 }
+
 
